4733d634066eb23552d859aab102c26864651f9e
Yeonoc <chlqhtjr752@kookmin.ac.kr>
2022-09-12 14:21:04 +0900

    test : notification

--- booriya/lib/pages/fire_off_page/fire_off.dart
+++ booriya/lib/pages/fire_off_page/fire_off.dart
@@ -1,7 +1,65 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class FireOff extends StatelessWidget {
+class FireOff extends StatefulWidget {
+  const FireOff({Key? key}) : super(key: key);
+
+  @override
+  State<FireOff> createState() => _FireOffState();
+}
+
+class _FireOffState extends State<FireOff> {
+  var _flutterLocalNotificationsPlugin;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+    var initializationSettingsIOS = IOSInitializationSettings();
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    _flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  //알림을 눌렀을때 어떤 행동을 할지 정해주는 부분
+  Future onSelectNotification(String payload) async {
+    print("payload : $payload");
+    showDialog(
+      context: context,
+      builder: (_) => AlertDialog(
+        title: Text('우왕 잘됩니다!!!!우와아아아아아아앙!!!'),
+        content: Text('Payload: $payload'),
+      ),
+    );
+  }
+
+  Future<void> _showNotification() async {
+    print("dkdkd");
+    var android = AndroidNotificationDetails(
+        'your channel id', 'your channel name',
+        channelDescription: 'your channel description',
+        importance: Importance.max,
+        priority: Priority.high);
+
+    var ios = IOSNotificationDetails();
+    var detail = NotificationDetails(android: android, iOS: ios);
+
+    await _flutterLocalNotificationsPlugin.show(
+      0,
+      '단일 Notification',
+      '단일 Notification 내용',
+      detail,
+      payload: 'Hello Flutter',
+    );
+    print('dfdf');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +69,8 @@ class FireOff extends StatelessWidget {
       body: Center(
         child: InkWell(
           onTap: () {
-            Navigator.pushNamed(context, "/on");
+            _showNotification();
+            // Navigator.pushNamed(context, "/on");
           },
           child: Container(
             width: 300,
